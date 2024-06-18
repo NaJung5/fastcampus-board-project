fb0e52c17a382704a5bbe5f5aae5a165e4fdedf4
나정호 <njung2516@gmail.com>
2024-06-18 16:49:09 +0900

    #8 - 도메인 설계
    댓글 도메인 오타 수정

--- src/main/java/com/fastcampus/fastcampusboardproject/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/fastcampusboardproject/domain/ArticleComment.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
-    private Article Article; // 게시글  (ID)
+    private Article article; // 게시글  (ID)
     private String content; // 본문
 
     private LocalDateTime createdAt; // 생성일시
